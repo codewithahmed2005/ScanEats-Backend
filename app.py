bfaaac19c67bb3875d3e86a9f10212b12c0c07a5
codewithahmed2005 <codewithahmed2005@gmail.com>
2026-07-14 17:14:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -292,9 +292,5 @@ def get_public_menu(restaurant_id):
     })
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-        print("✅ Database initialized!")
-
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
